8896eb697614594703de4ea391a23d1b757b0eef
Jose l. Azagra <azagramac@gmail.com>
2021-07-20 13:35:38 +0200

    Create DoinoCoin_ArduinoSlave.ino

---
+++ Arduino/DoinoCoin_ArduinoSlave.ino
@@ -0,0 +1,23 @@
+/*
+  DuinoCoin_ESP_Master.ino
+  created 10/05/2021
+  by Luiz H. Cassettari
+*/
+
+void setup() {
+  Serial.begin(115200);
+  DuinoCoin_setup();
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  if (DuinoCoin_loop())
+  {
+    Serial.print(F("Job Done: "));
+    Serial.print(DuinoCoin_response());
+  }
+  if (runEvery(250))
+  {
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+  }
+}
